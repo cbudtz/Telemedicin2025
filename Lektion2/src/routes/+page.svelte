48a0d0119e7ca8ade6b9be8697ea8c5978cac4be
cbudtz <chrbudtz@gmail.com>
2025-03-13 17:02:15 +0100

    Lektion2

---
+++ Lektion2/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let userName = $state('');
+	let password = $state('');
+	const onclick = () => {
+		if (userName === 'test' && password === 'test') {
+			alert('You are logged in');
+		} else {
+			alert('You are not logged in');
+		}
+	};
+</script>
+
+<div class="text-2xl">This is a page with a plus sign in the URL</div>
+<input
+	type="text"
+	class="rounded-md border border-gray-300 p-2"
+	placeholder="Type something here"
+	bind:value={userName}
+/>
+<input
+	type="password"
+	class="rounded-md border border-gray-300 p-2"
+	placeholder="Type something here"
+	bind:value={password}
+/>
+<button {onclick}>Click me</button>
